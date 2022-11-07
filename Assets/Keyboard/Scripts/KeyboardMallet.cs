61983d910da5aee6233dfb154d1a8afddf30d71a
spinaljack <spinaljack@users.noreply.github.com>
2022-11-07 23:56:18 +0000

    Convert from SteamVR to OpenXR
    Co-authored-by: mechabit <kevin.tsang@mechabit.co.uk>

--- Assets/Keyboard/Scripts/KeyboardMallet.cs
+++ Assets/Keyboard/Scripts/KeyboardMallet.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Valve.VR;
+using UnityEngine.XR;
+
 
 namespace Normal.UI {
     [RequireComponent(typeof(Rigidbody))]
     public class KeyboardMallet : MonoBehaviour {
         [SerializeField]
-        private SteamVR_TrackedObject _trackedObject;
+        private Transform _trackedObject;
+        [SerializeField]
+        private XRNode xRNode;
 
         [SerializeField]
         private Transform _head;
@@ -25,35 +28,6 @@ namespace Normal.UI {
             rigidbody.useGravity  = false;
             rigidbody.isKinematic = true;
             rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-        }
-
-        // Watch for new poses event from SteamVR.
-        void OnEnable() {
-            SteamVR_Events.NewPoses.Listen(OnNewPoses);
-        }
-
-        void OnDisable() {
-            SteamVR_Events.NewPoses.Remove(OnNewPoses);
-        }
-
-        private void OnNewPoses(TrackedDevicePose_t[] poses) {
-            if (_trackedObject.index == SteamVR_TrackedObject.EIndex.None)
-                return;
-
-            int i = (int)_trackedObject.index;
-            if (poses.Length <= i)
-                return;
-
-            if (!poses[i].bDeviceIsConnected)
-                return;
-
-            if (!poses[i].bPoseIsValid)
-                return;
-
-            SteamVR_Utils.RigidTransform pose = new SteamVR_Utils.RigidTransform(poses[i].mDeviceToAbsoluteTracking);
-            Vector3    worldPosition = _trackedObject.transform.parent.TransformPoint(pose.pos);
-            Quaternion worldRotation = _trackedObject.transform.parent.rotation * pose.rot;
-            PositionGeometry(worldPosition, worldRotation);
         }
 
         // Position this object at the center of the controller that we're tracking.
@@ -90,25 +64,26 @@ namespace Normal.UI {
             // I want the value from the previous frame.
             malletHeadPosition = _newMalletHeadPosition;
             _newMalletHeadPosition = _head.transform.position;
+
+            Vector3 worldPosition = _trackedObject.position;
+            Quaternion worldRotation = _trackedObject.rotation;
+            PositionGeometry(worldPosition, worldRotation);
         }
 
         // Haptic pulse
         void TriggerHapticPulse() {
-            StartCoroutine(HapticPulse());
-        }
-
-        IEnumerator HapticPulse() {
-            if (_trackedObject.index == SteamVR_TrackedObject.EIndex.None) {
-                yield break;
+            var device = InputDevices.GetDeviceAtXRNode(xRNode);
+            HapticCapabilities capabilities;
+            if (device.TryGetHapticCapabilities(out capabilities))
+            {
+                if (capabilities.supportsImpulse)
+                {
+                    uint channel = 0;
+                    float amplitude = 0.5f;
+                    float duration = 0.1f;
+                    device.SendHapticImpulse(channel, amplitude, duration);
+                }
             }
-
-            SteamVR_Controller.Device device = SteamVR_Controller.Input((int)_trackedObject.index);
-            // When I wrote this, two frames of vibration felt like a really solid hit without it feeling sloppy.
-            // Also using WaitForEndOfFrame() to match my implementation for the Oculus Touch controllers. Not sure if this is ideal.
-            device.TriggerHapticPulse(1500);
-            yield return new WaitForEndOfFrame();
-            device.TriggerHapticPulse(1500);
-            yield return new WaitForEndOfFrame();
         }
     }
 }
